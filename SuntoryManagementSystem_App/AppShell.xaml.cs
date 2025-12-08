a989c4177eddea0fb0cbd69aabbd4066bc288e90
zaka-krc <zakariakorchi2003@hotmail.com>
2025-12-08 21:59:15 +0100

    Initial create Web app + MAUI app

---
+++ SuntoryManagementSystem_App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SuntoryManagementSystem_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
